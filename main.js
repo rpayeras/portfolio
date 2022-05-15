a2158adeedef384b39a99de914f6b42d61ceae1f
rpayeras <payerasam@gmail.com>
2022-05-15 12:30:49 +0200

    Adjust js

--- main.js
+++ main.js
@@ -13,19 +13,22 @@ const initEvents = () => {
   });
 };
 
-initEvents();
+const initSlides = () => {
+  const slideOpts = {
+    type: "fade",
+    rewind: true,
+    autoplay: true,
+    pagination: false,
+    arrows: false,
+    lazyLoad: true,
+  };
 
-const slideOpts = {
-  type: "fade",
-  rewind: true,
-  autoplay: true,
-  pagination: false,
-  arrows: false,
-  lazyLoad: true,
+  const slideElms = document.getElementsByClassName("splide");
+
+  for (let i = 0; i < slideElms.length; i++) {
+    new Splide(slideElms[i], slideOpts).mount();
+  }
 };
 
-const slideElms = document.getElementsByClassName("splide");
-
-for (let i = 0; i < slideElms.length; i++) {
-  new Splide(slideElms[i], slideOpts).mount();
-}
+initSlides();
+initEvents();
