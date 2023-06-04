6ac5b3c44c52228c541315e74916ef84b124875e
Roberto Payeras <payerasam@gmail.com>
2023-06-04 13:02:45 +0200

    Update dependencies

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 import "./style.css";
 
-import Splide from "@splidejs/splide";
-// import "@splidejs/splide/css/skyblue";
+// import Splide from "@splidejs/splide";
+import "@splidejs/splide/css/skyblue";
 
-const initEvents = () => {
-  document.getElementById("menu__btn--open").addEventListener("click", () => {
-    document.getElementById("menu__wrapper").classList.remove("hidden");
-  });
+// const initEvents = () => {
+//   document.getElementById("menu__btn--open").addEventListener("click", () => {
+//     document.getElementById("menu__wrapper").classList.remove("hidden");
+//   });
 
-  document.getElementById("menu__btn--close").addEventListener("click", () => {
-    document.getElementById("menu__wrapper").classList.add("hidden");
-  });
-};
+//   document.getElementById("menu__btn--close").addEventListener("click", () => {
+//     document.getElementById("menu__wrapper").classList.add("hidden");
+//   });
+// };
 
-const initSlides = () => {
-  const slideOpts = {
-    type: "fade",
-    rewind: true,
-    autoplay: true,
-    pagination: false,
-    arrows: false,
-    lazyLoad: true,
-  };
+// const initSlides = () => {
+//   const slideOpts = {
+//     type: "fade",
+//     rewind: true,
+//     autoplay: true,
+//     pagination: false,
+//     arrows: false,
+//     lazyLoad: true,
+//   };
 
-  const slideElms = document.getElementsByClassName("splide");
+//   const slideElms = document.getElementsByClassName("splide");
 
-  for (let i = 0; i < slideElms.length; i++) {
-    new Splide(slideElms[i], slideOpts).mount();
-  }
-};
+//   for (let i = 0; i < slideElms.length; i++) {
+//     new Splide(slideElms[i], slideOpts).mount();
+//   }
+// };
 
-initSlides();
-initEvents();
+// initSlides();
+// initEvents();
